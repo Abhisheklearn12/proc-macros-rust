7eb36c386345e161ad27bd1318bdafc02487ce75
Abhisheklearn12 <cseabhishek123@gmail.com>
2025-11-08 19:42:33 +0530

    learnt about proc-macros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Tried to understand "procedural macros", here's the points:
+// 1. Procedural macros are not like runtime functions.
+// 2. They’re compile-time code transformers.
+//
+// 3. They:
+//
+// -Run before the compiler’s semantic phases,
+//
+// -Only see and emit syntax trees, not types,
+//
+// -Must generate valid Rust source,
+//
+// -Can easily create type errors or syntax errors if misused.
